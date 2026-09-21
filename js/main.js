474d1b590278d96906e75ede9391525694b3c694
HIRO-0727 <eandl.ads.jp@gmail.com>
2026-09-21 14:37:16 +0900

    ピクパPC_COMPANYまで完了

--- js/main.js
+++ js/main.js
@@ -36,10 +36,11 @@ window.addEventListener("DOMContentLoaded", () => {
         loopedSlides: 2,
         slidesPerView: "auto",
         speed: 8000,
-        autoplay: {
-            delay: 0,
-            disableOnInteraction: false,
-        },
+
+        // autoplay: {
+        //     delay: 0,
+        //     disableOnInteraction: false,
+        // },
     });
 });
 
