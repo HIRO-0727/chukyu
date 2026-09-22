6cf57db506dcf931d73b2c431c0883f2a0345345
HIRO-0727 <eandl.ads.jp@gmail.com>
2026-09-22 14:23:41 +0900

    ピクパ再チェック完了2

--- js/main.js
+++ js/main.js
@@ -11,14 +11,6 @@ $("#js-hamburger").click(function() {
     $("#js-overlay").toggleClass("is-open");
 });
 
-// 黒背景クリックでメニューを閉じる
-$("#js-overlay").click(function() {
-    if (!$menu.hasClass("is-open")) return;
-
-    $(".hamburger").removeClass("is-active");
-    $menu.removeClass("is-open").toggleClass("is-close", true);
-    $("#js-overlay").removeClass("is-open");
-});
 
 // 閉じるアニメーションが終わったら外す
 $menu.on("transitionend", function(event) {
